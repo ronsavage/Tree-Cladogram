d50e69101a9751a4590b15bff77640474656a262
ronsavage <ron@savage.net.au>
2015-12-30 17:25:28 +1100

    Move more Imager-specific code into Tree::Cladogram::Imager

--- lib/Tree/Cladogram/Imager.pm
+++ lib/Tree/Cladogram/Imager.pm
@@ -90,6 +90,145 @@ sub create_image
 
 # ------------------------------------------------
 
+sub draw_horizontal_branch
+{
+	my($self, $image, $middle_attributes, $daughter_attributes, $final_offset) = @_;
+	my($branch_color)	= $self -> branch_color;
+	my($branch_width)	= $self -> branch_width - 1;
+	my($x_step)			= $self -> x_step;
+
+	$image -> box
+	(
+		box =>
+		[
+			$$middle_attributes{x},
+			$$daughter_attributes{y},
+			$$daughter_attributes{x} + $x_step + $final_offset,
+			$$daughter_attributes{y} + $branch_width,
+		],
+		color	=> $branch_color,
+		filled	=> 1,
+	);
+
+} # End of draw_horizontal_branch.
+
+# ------------------------------------------------
+
+sub draw_leaf_name
+{
+	my($self, $image, $name, $daughter_attributes, $final_offset) = @_;
+
+	if ( (length($name) > 0) && ($name !~ /^\d+$/) )
+	{
+		my($bounds) 	= $$daughter_attributes{bounds};
+		$$bounds[0]		+= $final_offset;
+		$$bounds[2]		+= $final_offset;
+		my($fuschia)	= Imager::Color -> new(0xff, 0, 0xff);
+
+		$image -> string
+		(
+			align	=> 0,
+			font	=> $self -> leaf_font,
+			string	=> $name,
+			x		=> $$bounds[0],
+			y		=> $$bounds[1],
+		);
+
+		if ($self -> debug && 0)
+		{
+			$image -> box
+			(
+				box		=> $bounds,
+				color	=> $fuschia,
+				filled	=> 0,
+			);
+		}
+	}
+
+} # End of draw_leaf_name.
+
+# ------------------------------------------------
+
+sub draw_root_branch
+{
+	my($self, $image)			= @_;
+	my($branch_color)			= $self -> branch_color;
+	my($branch_width)			= $self -> branch_width - 1;
+	my($attributes)				= $self -> root -> attributes;
+	my(@daughters)				= $self -> root -> daughters;
+	my($daughter_attributes)	= $daughters[0] -> attributes;
+
+	$image -> box
+	(
+		box =>
+		[
+			$$daughter_attributes{x},
+			$$daughter_attributes{y},
+			$self -> left_margin,
+			$$attributes{y} + $branch_width,
+		],
+		color	=> $branch_color,
+		filled	=> 1,
+	);
+
+} # End of draw_root_branch.
+
+# ------------------------------------------------
+
+sub draw_title
+{
+	my($self, $image, $maximum_x, $maximum_y) = @_;
+	my($title) = $self -> title;
+
+	if (length($title) > 0)
+	{
+		my(@bounds) = $self -> title_font -> align
+						(
+							halign	=> 'left',
+							image	=> undef,
+							string	=> $title,
+							valign	=> 'baseline',
+							x		=> 0,
+							y		=> 0,
+						);
+
+		$image -> string
+		(
+			align	=> 0,
+			font	=> $self -> title_font,
+			string	=> $title,
+			x		=> int( ($maximum_x - $bounds[2]) / 2),
+			y		=> $maximum_y - $self -> top_margin,
+		);
+	}
+
+} # End of draw_title.
+
+# ------------------------------------------------
+
+sub draw_vertical_branch
+{
+	my($self, $image, $middle_attributes, $daughter_attributes) = @_;
+	my($branch_color)	= $self -> branch_color;
+	my($branch_width)	= $self -> branch_width - 1;
+
+	$image -> box
+	(
+		box =>
+		[
+			$$middle_attributes{x},
+			$$middle_attributes{y},
+			$$middle_attributes{x} + $branch_width,
+			$$daughter_attributes{y},
+		],
+		color	=> $branch_color,
+		filled	=> 1,
+	);
+
+} # End of draw_vertical_branch.
+
+# ------------------------------------------------
+
 sub place_text
 {
 	my($self)			= @_;
